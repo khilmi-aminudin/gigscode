9de283f10c3fdc2ac2b626fbae2e9a78313184cc
Khilmi Aminudin <khilmiaminudin715@gmail.com>
2020-11-11 11:37:39 +0700

    move dependencies to development

--- routes/gigs.js
+++ routes/gigs.js
@@ -19,87 +19,64 @@ router.get('/add', (req,res) => {
 })
 
 router.post('/add', (req,res) => {
-    db.gig.findAll()
-    .then(gigs => {
-        res.render('gigs',{
-            gigs
-        })
-    })
-    .catch(err => {
-        console.error(err)
-    })
-})
-
-// Add a gig form
-router.get('/add', (req,res) => {
-    res.render('add')
-})
-
-// Add a gig
-router.post('/add', (req,res) => {
-
     let {title,technologies,budget,description,contact_email} = req.body
     let errors = {}
 
-    console.log(req.body)
+    if(!title){
+        errors.title = 'Please Add a Title'
+    }
+    if(!technologies){
+        errors.technologies = 'Please Add a technologies'
+    }
+    if(!budget){
+        errors.budget = 'Please Add a budget'
+    }
+    if(!description){
+        errors.description = 'Please Add a description'
+    }
+    if(!contact_email){
+        errors.contact_email = 'Please Add a contact_email'
+    }
 
-    // validate fields
-    // if(!title){
-    //     errors.title =  'Please add a title'
-    // }
-    // if(!technologies){
-    //     errors.technologies = 'Please add some technologies'
-    // }
-    // if(!description){
-    //     errors.description = 'Please add a description'
-    // }
-    // // if(!budget){
-    // //     errors.budget = 'Please add the budget'
-    // // }
-    // if(!contact_email){ 
-    //     errors.contact_email = 'Please add a contact_email'
-    // }
+    const isError = Object.keys(errors).length === 0 && errors.constructor === Object
 
-    // const isEmptyErrors = Object.keys(errors).length === 0 && errors.constructor === Object
-    // // check for errors
-    // if(!isEmptyErrors){
-    //     res.render('add',{
-    //         errors,
-    //         title,
-    //         technologies,
-    //         budget,
-    //         description,
-    //         contact_email
-    //     })
-    // }else{
-    //     if(!budget){
-    //         budget = 'unknown'
-    //     }else{
-    //         budget = `$${budget}`
-    //     }
+    if(!isError){
+        res.render('add',{
+            errors,
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        })
+    }else{
+        if(!budget){
+            budget = 'unknown'
+        }else{
+            budget = `$${budget}`
+        }
 
-    //     // Make lowercase and remove space after coma
-    //     technologies = technologies.toLowerCase().replace(/, /g, ',')
+        // Make lowercase and remove space after coma
+        technologies = technologies.toLowerCase().replace(/, /g, ',')
 
-    //     // insert into table
-    //     db.gig.create({
-    //         title,
-    //         technologies,
-    //         budget,
-    //         description,
-    //         contact_email
-    //     })
-    //     .then(() => res.redirect('/gigs'))
-    //     .catch(err => console.log(err))
-    // }
+        // insert into table
+        db.gig.create({
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        })
+        .then(() => res.redirect('/gigs'))
+        .catch(err => console.log(err))
+    }
 
 })
 
-// Search a Gigs
 router.get('/search', (req,res) => {
     const { term } = req.query
 
-    db.gig.findAll({ where : { technologies : { [Op.like] : '%'+ term +'%'}}})
+    Gig.findAll({ where : { technologies : { [Op.like] : '%'+ term +'%'}}})
         .then(gigs => {
             res.render('gigs', {
                 gigs
@@ -107,5 +84,6 @@ router.get('/search', (req,res) => {
         })
         .catch(err => console.log(err))
 })
+
 
 module.exports = router
